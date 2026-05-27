309f1bf541a7c6255b6872eed576f93993a89f53
omprakash <omprakashn@bitlasoft.com>
2026-05-27 17:58:24 +0530

    push code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1279554487" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1397602156" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
